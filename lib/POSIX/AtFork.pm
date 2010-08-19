be7bf4a87561dea522feef96a72deaea95d1ae85
Fuji, Goro <gfuji@cpan.org>
2010-08-19 16:08:45 +0900

    Docs

--- lib/POSIX/AtFork.pm
+++ lib/POSIX/AtFork.pm
@@ -1,40 +1,21 @@
 package POSIX::AtFork;
-
-use 5.008005;
+use 5.008001;
 use strict;
 use warnings;
 
-require Exporter;
+our $VERSION = '0.01';
 
+require Exporter;
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use POSIX::AtFork ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
+our @EXPORT_OK = qw(pthread_atfork);
 
 require XSLoader;
 XSLoader::load('POSIX::AtFork', $VERSION);
 
-# Preloaded methods go here.
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
@@ -61,26 +42,17 @@ None by default.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<pthread_atfork(3)>.
 
 =head1 AUTHOR
 
-CentOS, E<lt>goro@localdomainE<gt>
+Fuji, Goro (gfx)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 by CentOS
+Copyright (C) 2010 by Fuji, Goro gfx E<lt>gfuji@cpan.orgE<gt>. 
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.5 or,
-at your option, any later version of Perl 5 you may have available.
-
+it under the same terms as Perl itself,
 
 =cut
